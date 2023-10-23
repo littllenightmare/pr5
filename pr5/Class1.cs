80a74d7df18b3b99c31fe9360f13dad43596e9ab
исп-31 <-31@LAB17-13>
2023-10-23 12:39:02 +0300

    Добавьте файлы проекта.

---
+++ pr5/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr5
+{
+    class Pair
+    {
+        int a, b, c, d, e, f;
+        public int[] firstpair { get; set; }
+        public int[] secondpair { get; set; }
+        public int[] thirdpair { get; set; }
+
+        public Pair (int a, int b)
+        {
+           
+        }
+        /// <summary>
+        /// Функция для сравнения чисел в паре
+        /// </summary>
+        /// <param name="A">первое число первой пары</param>
+        /// <param name="B">второе число первой пары</param>
+        /// <param name="C">первое число второй пары</param>
+        /// <param name="D">второе число второй пары</param>
+        /// <returns>строка с указанием равенства или неравенства</returns>
+        public string equals(int A, int B, int C, int D)
+        {
+            string first, second;
+            if (A == B) first = "равна"; else first = "не равна";
+            if (C == D) second = "равна"; else second = "не равна";
+            string info = "Первая пара " + first + ", вторая пара " + second;
+            return info;
+        }
+        /// <summary>
+        /// Функция для умножения чисел в паре
+        /// </summary>
+        /// <param name="a">первое число первой пары</param>
+        /// <param name="b">второе число первой пары</param>
+        /// <param name="c">первое число второй пары</param>
+        /// <param name="d">второе число второй пары</param>
+        /// <param name="first">умножение первой пары</param>
+        /// <param name="second">умножение второй пары</param>
+        public void multiply(int a, int b, int c, int d, out int first, out int second)
+        {
+           first = a*b; second = d*c;
+        }
+    }
+}
---
+++ pr5/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void infoclick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая 5 Кульковой А.\r\nСоздать базовый класс Pair (пара целых чисел). Создать необходимые методы и свойства.\r\n Определить методы с операциями проверки на равенство и перемножения полей.\r\n Реализовать операцию вычитания пар по формуле (а, b) - (с, d) = (а - c, b - d).\r\n Создать перегруженный метод для вычитания трех пар чисел.");
+        }
+
+        private void exClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
